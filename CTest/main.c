2cf69d96a5182c704779be0877861806c271bf95
Simone Longo <simonelongo@simones-mbp.uconnect.utah.edu>
2018-05-31 10:35:32 -0600

    Combinatorics done! No longer outputs result of factorial.

--- CTest/main.c
+++ CTest/main.c
@@ -14,19 +14,33 @@ long nCk(int, int);
 long nPk(int, int);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     int n, k;
-    //long nck, npk;
+    long nck, npk;
     
     n = atoi(argv[1]);
-    //k = atoi(argv[1]);
+    k = atoi(argv[2]);
     
-    //nck = nCk(n, k);
-    //npk = nPk(n, k);
-    printf("%d! = %ld\n\n", n, factorial(n));
-    //printf("%d choose %d equals %ld\n\n", n, k, nck);
-    //printf("%d nPk %d equals %ld\n", n, k, npk);
+    if (k > n){
+        printf("Invalid arguments/n");
+        exit(0);
+    }
+    
+    nck = nCk(n, k);
+    npk = nPk(n, k);
+    //printf("%d! = %ld\n\n", n, factorial(n));
+    printf("%d choose %d equals %ld\n\n", n, k, nck);
+    printf("%d nPk %d equals %ld\n", n, k, npk);
     return 0;
+}
+
+long nCk(int n, int k){
+    long nck = factorial(n)/(factorial(n-k)*factorial(k));
+    return nck;
+}
+
+long nPk(int n, int k){
+    long npk = factorial(n)/factorial(n-k);
+    return npk;
 }
 
 long factorial(int n){
