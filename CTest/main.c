a909ed4be11edc808863b4ef7066309a40e617da
Simone Longo <simonelongo@simones-mbp.uconnect.utah.edu>
2018-05-31 10:23:03 -0600

    Factorial working!

--- CTest/main.c
+++ CTest/main.c
@@ -7,9 +7,33 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+long factorial(int);
+long nCk(int, int);
+long nPk(int, int);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int n, k;
+    //long nck, npk;
+    
+    n = atoi(argv[1]);
+    //k = atoi(argv[1]);
+    
+    //nck = nCk(n, k);
+    //npk = nPk(n, k);
+    printf("%d! = %ld\n\n", n, factorial(n));
+    //printf("%d choose %d equals %ld\n\n", n, k, nck);
+    //printf("%d nPk %d equals %ld\n", n, k, npk);
     return 0;
+}
+
+long factorial(int n){
+    long result = 1;
+    while((n) > 0){
+        result *= n;
+        n--;
+    }
+    return result;
 }
